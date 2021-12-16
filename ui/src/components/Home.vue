8e5486f55787de897f8f2fca7812dca47d3a1cfc
Tim Hiatt <timhiatt@google.com>
2021-12-16 16:46:23 +0000

    Adding first content to the site

---
+++ ui/src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+    <div
+      class="relative z-30 p-3 px-12 text-2xl text-white bg-gray-300 bg-opacity-30 rounded-xl"
+    >
+      TJHiatt
+    </div>
+    <video
+      autoplay
+      loop
+      muted
+      class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+    >
+      <source
+        src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
+        type="video/mp4"
+      />
+      Your browser does not support the video tag.
+    </video>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue';
 import './registerServiceWorker';
 import store from './store';
 
+import './index.css';
+
 createApp(App).use(store).mount('#app');
